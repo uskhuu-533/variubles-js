a5fc06efe6d2131840ae3e31c19b67024172e303
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-02 17:15:09 +0800

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello from JS Variables")
+
+const firstName = "Bat";
+console.log(firstName);
+
+const lastName = "Bold";
+console.log(lastName)
+
+const maritalStatus = false;
+console.log(maritalStatus);
+
+const country = "Mongolia";
+console.log(country);
+
+const age = 18;
+console.log(age);
+
+console.log(firstName, lastName, maritalStatus, country, age)
+
+
